7b31e95ef77fad855641e8f9e1a7bd27d8ddb128
0707Felix <jackyboy9898>
2025-01-17 10:55:38 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:'/https://github.com/0707Felix/React_W3_SkilledReact.git/'
+})
